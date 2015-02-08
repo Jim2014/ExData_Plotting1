8875f3604edd1da3d138ef93ae43b356afc3e524
Jim2014 <jim_zhoujin@163.com>
2015-02-08 10:55:32 +0800

    update

--- plot3.R
+++ plot3.R
@@ -18,17 +18,10 @@ plot3<-function()
   # draw Plot 
   par(mfrow = c(1, 1))
   
-  #plot( data$DateTime, data$Sub_metering_1, type ='l', xlab="", ylab="Energy Sub metering")
-  #lines(data$DateTime, data$Sub_metering_2, type ='l',col="red")
-  #lines(data$DateTime, data$Sub_metering_3, type ='l',col="blue")
-  #legend("topright",  col = c("black", "blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=1)
-  
   plot( data$DateTime, data$Sub_metering_1, type ='l', xlab="", ylab="Energy Sub metering")
-  lines(data$DateTime, data$Sub_metering_2, col="red")
-  lines(data$DateTime, data$Sub_metering_3, col="blue")
-  legend_text=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3") 
-  legend("topright", lty=1, lwd=1, col = c("black", "blue", "red"), legend = legend_text, cex=0.8)
-  
+  lines(data$DateTime, data$Sub_metering_2, type ='l',col="red")
+  lines(data$DateTime, data$Sub_metering_3, type ='l',col="blue")
+  legend("topright",  col = c("black", "blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=1)
   
   dev.copy(png, file = "plot3.png", width = 480, height = 480)
   dev.off()
